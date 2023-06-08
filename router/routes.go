450660ac38438c705ba64f4567cdcd72246d0ee6
Vitor <vi.souza.almeida@protonmail.com>
2023-06-07 21:04:11 -0300

    feat(api): swagger configuration

--- router/routes.go
+++ router/routes.go
@@ -2,11 +2,18 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	docs "github.com/vit0rr/jub/docs"
 	"github.com/vit0rr/jub/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
+
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
@@ -15,4 +22,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
